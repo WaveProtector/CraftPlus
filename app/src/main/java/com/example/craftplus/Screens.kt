fda92931375e65db9681dcd5008d4add742d94d8
fc56380 <fc56380@alunos.fc.ul.pt>
2025-01-21 16:53:39 +0000

    added new temp screen

--- app/src/main/java/com/example/craftplus/Screens.kt
+++ app/src/main/java/com/example/craftplus/Screens.kt
@@ -6,6 +6,7 @@ sealed class Screens(val route: String) {
     object Home: Screens("home")
     object Builds: Screens("Build")
     object Search: Screens("search")
+    object Settings: Screens("settings")
     object Materials: Screens("materials")
     object Profile: Screens("profile")
     object Friends: Screens("friends")
